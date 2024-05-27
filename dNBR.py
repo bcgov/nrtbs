77fea32284179d0338558f24df7279ae1a611237
SterlingvonDehn <sterlingvondehn@gmail.com>
2024-05-27 08:13:25 -0700

    Update dNBR.py

--- dNBR.py
+++ dNBR.py
@@ -27,7 +27,7 @@ def NBR(file_name):
             B11[i][j] = data[width*height*1 + width*i+j]
             B09[i][j] = data[width*height*2 + width*i+j]
             B08[i][j] = data[width*height*3 + width*i+j]
-    return [B12,B11,B09,B08, NBR]
+    return [B12,B11,B09,B08,NBR]
             
             
 def dNBR(start_frame, end_frame):
@@ -35,8 +35,8 @@ def dNBR(start_frame, end_frame):
     Takes the start and end binary files and returns the dNRB.
     >>> dNBR('S2B_MSIL1C_20210626T185919_N0300_R013_T10UFB_20210626T211041.bin', 'S2A_MSIL1C_20210907T190911_N0301_R056_T10UFB_20210902T225534.bin')
     '''
-    preNBR = NBR(start_frame)[3]
-    postNBR = NBR(end_frame)[3]
+    preNBR = NBR(start_frame)[4]
+    postNBR = NBR(end_frame)[4]
     dNBR = postNBR - preNBR
     plt.imshow(dNBR, cmap='Greys')
     plt.show()
