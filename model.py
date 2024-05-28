fc942b1ccbe377dafe6ad4e8a8a589499962bb57
SterlingvonDehn <sterlingvondehn@gmail.com>
2024-05-28 12:01:13 -0700

    Update model.py

--- model.py
+++ model.py
@@ -1,9 +1,9 @@
-
 from sklearn.linear_model import LinearRegression 
 from dNBR import dNBR, NBR
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import os
 
 
 filenames = ['S2B_MSIL1C_20210626T185919_N0300_R013_T10UFB_20210626T211041.bin','S2B_MSIL1C_20210629T190919_N0300_R056_T10UFB_20210629T212050.bin','S2A_MSIL1C_20210701T185921_N0301_R013_T10UFB_20210701T223921.bin','S2B_MSIL1C_20210709T190919_N0301_R056_T10UFB_20210709T224644.bin','S2A_MSIL1C_20210714T190921_N0301_R056_T10UFB_20210714T225634.bin','S2B_MSIL1C_20210719T190919_N0301_R056_T10UFB_20210719T212141.bin','S2A_MSIL1C_20210724T190921_N0301_R056_T10UFB_20210724T230122.bin','S2B_MSIL1C_20210726T185919_N0301_R013_T10UFB_20210726T211239.bin','S2B_MSIL1C_20210729T190919_N0301_R056_T10UFB_20210729T212314.bin','S2A_MSIL1C_20210803T190921_N0301_R056_T10UFB_20210803T224926.bin','S2B_MSIL1C_20210805T185919_N0301_R013_T10UFB_20210805T211134.bin','S2A_MSIL1C_20210813T190921_N0301_R056_T10UFB_20210813T224901.bin','S2A_MSIL1C_20210902T190911_N0301_R056_T10UFB_20210902T225534.bin','S2B_MSIL1C_20210907T190929_N0301_R056_T10UFB_20210907T224046.bin']
@@ -40,20 +40,30 @@ def model(stop_index, filenames):
     reg = LinearRegression().fit(X, Y)
     pred = reg.predict(X)
     data = np.zeros((height,width))
+    score = reg.score(X, Y)
+    
+    date = filenames[stop_index].split('_')[2].split('T')[0]
     for n in range(len(pred)): #going through the prediction list to plot the predicted dNBR
         i = n // width
         j = n % height
         data[i][j] = pred[n]
     err = dnbr - data #error 
-    plt.imshow(err,cmap='Greys') 
-    plt.show()
-    plt.imshow(data,cmap='Greys')
+    plt.figure(figsize=(15,15))
+    
+    plt.imshow(err,cmap='Greys')
     plt.colorbar()
-    plt.show()
-    score = reg.score(X, Y)
-    print(score) 
+    plt.title(f'Error using stop date {date}. Score: {score}')
+    if not os.path.exists('dNBR_model_error'):
+        os.mkdir('dNBR_model_error')    
+    plt.savefig(f'dNBR_model_error/{date}_{filenames[stop_index]}.png')
+    
+    plt.imshow(data,cmap='Greys')
+    plt.title(f'Predicted dNBR using stop index {date}. Score: {score}')
+    if not os.path.exists('dNBR_model'):
+        os.mkdir('dNBR_model')    
+    plt.savefig(f'dNBR_model/{date}_{filenames[stop_index]}.png')
+    #print(score) 
  
  
 for i in range(len(filenames)):
     model(i, filenames) 
-
