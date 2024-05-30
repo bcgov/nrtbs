087545f106c90683290b364c3aa7a51bf67ad732
SterlingvonDehn <sterlingvondehn@gmail.com>
2024-05-30 14:19:06 -0700

    Add files via upload

--- mask.py
+++ mask.py
@@ -37,148 +37,68 @@ def param_plots(file_list,threshold):
     '''
     Makes plots of the burned and unburned mean and standard deveation of the 4 parameters calculated using the NBR function, B12, B11, B09, B08, and NBR.
     '''
-    mask = burnmask(file_list[0],file_list[-1],threshold) #calculating the mask ie. where is burned/unburned
+    burned_b12 = []; burned_b11 = []; burned_b09 = []; burned_b08 = []; burned_nbr = []; unburned_b12 = []; unburned_b11 = []; unburned_b09 = []; unburned_b08 = []; unburned_nbr = []
     
     burn12_mean = []; burn11_mean = []; burn09_mean = []; burn08_mean = []; burnnbr_mean = []; unburn12_mean = []; unburn11_mean = []; unburn09_mean = [];unburn08_mean = []; unburnnbr_mean = []
-    
+        
     burn12_dev = []; burn11_dev = []; burn09_dev = []; burn08_dev = []; burnnbr_dev = []; unburn12_dev = []; unburn11_dev = []; unburn09_dev = [];unburn08_dev = []; unburnnbr_dev = []
+        
+
+    burned_band_list = [burned_b12,burned_b11,burned_b09,burned_b08,burned_nbr]
+    unburned_band_list = [unburned_b12, unburned_b11, unburned_b09, unburned_b08, unburned_nbr]
+
+    
+    mean_burned_band_list = [burn12_mean, burn11_mean, burn09_mean, burn08_mean, burnnbr_mean]
+    mean_unburned_band_list = [unburn12_mean, unburn11_mean, unburn09_mean, unburn08_mean, unburnnbr_mean]
+    std_burned_band_list = [burn12_dev, burn11_dev, burn09_dev, burn08_dev, burnnbr_dev]
+    std_unburned_band_list = [unburn12_dev, unburn11_dev, unburn09_dev, unburn08_dev, unburnnbr_dev]
+    
+    mask = burnmask(file_list[0],file_list[-1],threshold) #calculating the mask ie. where is burned/unburned
     
     time = []
     
     start_date = datetime.datetime.strptime(file_list[0].split('_')[2].split('T')[0],'%Y%m%d') #fire start date
     
-    for file in file_list: #loops finding pixel values for each parameter and sorting them to burned or unburned
-        burned_b12 = []; burned_b11 = []; burned_b09 = []; burned_b08 = []; burned_nbr = []; unburned_b12 = []; unburned_b11 = []; unburned_b09 = []; unburned_b08 = []; unburned_nbr = []
+    for file in file_list:#loops finding pixel values for each parameter and sorting them to burned or unburned
+        for band in range(5):
+            burned_band_list[band] = []
+            unburned_band_list[band] = []
         
         date  = datetime.datetime.strptime(file.split('_')[2].split('T')[0],'%Y%m%d')
         params = NBR(f'raster_data/small/{file}')
         for i in range(len(mask[0])):
             for j in range(len(mask)):
-                if mask[i][j]:
-                    if not np.isnan(params[0][i][j]):
-                        burned_b12 += [params[0][i][j]]
-                    if not np.isnan(params[1][i][j]):
-                        burned_b11 += [params[1][i][j]]
-                    if not np.isnan(params[2][i][j]):
-                        burned_b09 += [params[2][i][j]]
-                    if not np.isnan(params[3][i][j]):
-                        burned_b08 += [params[3][i][j]]
-                    if not np.isnan(params[4][i][j]):
-                        burned_nbr += [params[4][i][j]]
-                else:
-                    if not np.isnan(params[0][i][j]):
-                        unburned_b12 += [params[0][i][j]]
-                    if not np.isnan(params[1][i][j]):
-                        unburned_b11 += [params[1][i][j]]
-                    if not np.isnan(params[2][i][j]):
-                        unburned_b09 += [params[2][i][j]]
-                    if not np.isnan(params[3][i][j]):
-                        unburned_b08 += [params[3][i][j]]
-                    if not np.isnan(params[4][i][j]):
-                        unburned_nbr += [params[4][i][j]]
-        burn12_mean += [np.mean(burned_b12)] #calculating means
-        burn11_mean += [np.mean(burned_b11)]
-        burn09_mean += [np.mean(burned_b09)]
-        burn08_mean += [np.mean(burned_b08)]
-        burnnbr_mean += [np.mean(burned_nbr)]
-        
-        burn12_dev += [np.std(burned_b12)] #calculating standard deveations
-        burn11_dev += [np.std(burned_b11)]
-        burn09_dev += [np.std(burned_b09)]
-        burn08_dev += [np.std(burned_b08)]
-        burnnbr_dev += [np.std(burned_nbr)]
-        
-        days = date.day - start_date.day #getting an estimate of the elapsed days
-        months = date.month - start_date.month 
-        time += [days + months*31]
+                for band in range(5):
+                    if mask[i][j]:
+                        burned_band_list[band] += [params[band][i][j]]
+                    else:
+                        unburned_band_list[band] += [params[band][i][j]]
+                        
+        for band in range(5):
+            mean_burned_band_list[band] += [np.nanmean(burned_band_list[band])]
+            std_burned_band_list[band] += [np.nanstd(burned_band_list[band])]
             
-        unburn12_mean += [np.mean(unburned_b12)]
-        unburn11_mean += [np.mean(unburned_b11)]
-        unburn09_mean += [np.mean(unburned_b09)]
-        unburn08_mean += [np.mean(unburned_b08)]
-        unburnnbr_mean += [np.mean(unburned_nbr)]
-        
-        unburn12_dev += [np.std(unburned_b12)]
-        unburn11_dev += [np.std(unburned_b11)]
-        unburn09_dev += [np.std(unburned_b09)]
-        unburn08_dev += [np.std(unburned_b08)]
-        unburnnbr_dev += [np.std(unburned_nbr)]
-        
+            mean_unburned_band_list[band] += [np.nanmean(unburned_band_list[band])]
+            std_unburned_band_list[band] += [np.nanstd(unburned_band_list[band])]    
+            
+        time += [date]
+
     plt.figure(figsize=(15,15)) #plotting
     
-    plt.plot(time, burn12_mean, color='red', label='Burned mean')
-    plt.plot(time, list(map(add,burn12_mean, burn12_dev)), color='red', linestyle='dashed')
-    plt.plot(time, list(map(sub,burn12_mean, burn12_dev)), color='red', linestyle='dotted')
+    band_title = ['B12', 'B11', 'B09', 'B08', 'NBR']
+    for band in range(5):
+        plt.plot(time, mean_burned_band_list[band], color='red',label='Burned mean')
+        plt.plot(time, list(map(add,mean_burned_band_list[band], std_burned_band_list[band])), color='red', linestyle='dashed', label='Burned mean +')
+        plt.plot(time, list(map(sub,mean_burned_band_list[band], std_burned_band_list[band])), color='red', linestyle='dotted', label='Burned mean -')
         
-    plt.plot(time, unburn12_mean, color='green', label='Unburned mean')
-    plt.plot(time, list(map(add,unburn12_mean, unburn12_dev)), color='green', linestyle='dashed')
-    plt.plot(time, list(map(sub,unburn12_mean, unburn12_dev)), color='green', linestyle='dotted')      
-    plt.legend()
-    plt.title(f'B12 burned and unburned')
-    plt.tight_layout()
-    plt.savefig(f'B12.png')
-    plt.clf()
+        plt.plot(time, mean_unburned_band_list[band], color='green',label='Unburned mean')
+        plt.plot(time, list(map(add,mean_unburned_band_list[band], std_unburned_band_list[band])), color='green', linestyle='dashed', label='Unburned mean +')
+        plt.plot(time, list(map(sub,mean_unburned_band_list[band], std_unburned_band_list[band])), color='green', linestyle='dotted', label='Unburned mean -')            
+        plt.legend()
+        plt.title(f'{band_title[band]} burned and unburned')
+        plt.tight_layout()
+        plt.savefig(f'{band_title[band]}.png')
+        plt.clf()
     
-    
-    
-    plt.plot(time, burn11_mean, color='red', label='Burned mean')
-    plt.plot(time, list(map(add,burn11_mean, burn11_dev)), color='red', linestyle='dashed')
-    plt.plot(time, list(map(sub,burn11_mean, burn11_dev)), color='red', linestyle='dotted')
-        
-    plt.plot(time, unburn11_mean, color='green', label='Unburned mean')
-    plt.plot(time, list(map(add,unburn11_mean, unburn11_dev)), color='green', linestyle='dashed')
-    plt.plot(time, list(map(sub,unburn11_mean, unburn11_dev)), color='green', linestyle='dotted')      
-    plt.legend()
-    plt.title(f'B11 burned and unburned')
-    plt.tight_layout()
-    plt.savefig(f'B11.png')
-    plt.clf()
-    
-    
-    
-    plt.plot(time, burn09_mean, color='red', label='Burned mean')
-    plt.plot(time, list(map(add,burn09_mean, burn09_dev)), color='red', linestyle='dashed')
-    plt.plot(time, list(map(sub,burn09_mean, burn09_dev)), color='red', linestyle='dotted')
-        
-    plt.plot(time, unburn09_mean, color='green', label='Unburned mean')
-    plt.plot(time, list(map(add,unburn09_mean, unburn09_dev)), color='green', linestyle='dashed')
-    plt.plot(time, list(map(sub,unburn09_mean, unburn09_dev)), color='green', linestyle='dotted')      
-    plt.legend()
-    plt.title(f'B09 burned and unburned')
-    plt.tight_layout()
-    plt.savefig(f'B09.png') 
-    plt.clf()
-    
-    
-    
-    plt.plot(time, burn08_mean, color='red', label='Burned mean')
-    plt.plot(time, list(map(add,burn08_mean, burn08_dev)), color='red', linestyle='dashed')
-    plt.plot(time, list(map(sub,burn08_mean, burn08_dev)), color='red', linestyle='dotted')
-        
-    plt.plot(time, unburn08_mean, color='green', label='Unburned mean')
-    plt.plot(time, list(map(add,unburn08_mean, unburn08_dev)), color='green', linestyle='dashed')
-    plt.plot(time, list(map(sub,unburn08_mean, unburn08_dev)), color='green', linestyle='dotted')      
-    plt.legend()
-    plt.title(f'B08 burned and unburned')
-    plt.tight_layout()
-    plt.savefig(f'B08.png')   
-    plt.clf()
-    
-    
-    
-    plt.plot(time, burnnbr_mean, color='red', label='Burned mean')
-    plt.plot(time, list(map(add,burnnbr_mean, burnnbr_dev)), color='red', linestyle='dashed')
-    plt.plot(time, list(map(sub,burnnbr_mean, burnnbr_dev)), color='red', linestyle='dotted')
-    
-    plt.plot(time, unburnnbr_mean, color='green', label='Unburned mean')
-    plt.plot(time, list(map(add,unburnnbr_mean, unburnnbr_dev)), color='green', linestyle='dashed')
-    plt.plot(time, list(map(sub,unburnnbr_mean, unburnnbr_dev)), color='green', linestyle='dotted')   
-    plt.legend()
-    plt.title(f'NBR burned and unburned')
-    plt.tight_layout()
-    plt.savefig(f'NBR.png')    
-    plt.clf()
-
-
 filenames = ['S2B_MSIL1C_20210626T185919_N0300_R013_T10UFB_20210626T211041.bin','S2B_MSIL1C_20210629T190919_N0300_R056_T10UFB_20210629T212050.bin','S2A_MSIL1C_20210701T185921_N0301_R013_T10UFB_20210701T223921.bin','S2B_MSIL1C_20210709T190919_N0301_R056_T10UFB_20210709T224644.bin','S2A_MSIL1C_20210714T190921_N0301_R056_T10UFB_20210714T225634.bin','S2B_MSIL1C_20210719T190919_N0301_R056_T10UFB_20210719T212141.bin','S2A_MSIL1C_20210724T190921_N0301_R056_T10UFB_20210724T230122.bin','S2B_MSIL1C_20210726T185919_N0301_R013_T10UFB_20210726T211239.bin','S2B_MSIL1C_20210729T190919_N0301_R056_T10UFB_20210729T212314.bin','S2A_MSIL1C_20210803T190921_N0301_R056_T10UFB_20210803T224926.bin','S2B_MSIL1C_20210805T185919_N0301_R013_T10UFB_20210805T211134.bin','S2A_MSIL1C_20210813T190921_N0301_R056_T10UFB_20210813T224901.bin','S2A_MSIL1C_20210902T190911_N0301_R056_T10UFB_20210902T225534.bin','S2B_MSIL1C_20210907T190929_N0301_R056_T10UFB_20210907T224046.bin']
 param_plots(filenames,.2)
