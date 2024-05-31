47bbcee921234be2cd8f149415606dff9356aaf8
Sterling von Dehn <sterlingvondehn@gmail.com>
2024-05-31 15:34:36 -0700

    updates

--- mask.py
+++ mask.py
@@ -16,13 +16,7 @@ def burnmask(start_file,end_file,threshold):
     width = vals[0]
     height = vals[1]
     dnbr = dNBR(f'raster_data/small/{start_file}',f'raster_data/small/{end_file}')
-    mask = np.zeros((height,width))
-    for i in range(height):
-        for j in range(width):
-            if dnbr[i][j] >= threshold: #selecting pixels with dNBR >= threshold and discading the rest
-                mask[i][j] = True
-            else:
-                mask[i][j] = False
+    mask = dnbr >= threshold
     '''
     plt.figure(figsize=(15,15))
     plt.imshow(mask, cmap='grey')
@@ -100,5 +94,7 @@ def param_plots(file_list,threshold):
         plt.savefig(f'{band_title[band]}.png')
         plt.clf()
     
+'''
 filenames = ['S2B_MSIL1C_20210626T185919_N0300_R013_T10UFB_20210626T211041.bin','S2B_MSIL1C_20210629T190919_N0300_R056_T10UFB_20210629T212050.bin','S2A_MSIL1C_20210701T185921_N0301_R013_T10UFB_20210701T223921.bin','S2B_MSIL1C_20210709T190919_N0301_R056_T10UFB_20210709T224644.bin','S2A_MSIL1C_20210714T190921_N0301_R056_T10UFB_20210714T225634.bin','S2B_MSIL1C_20210719T190919_N0301_R056_T10UFB_20210719T212141.bin','S2A_MSIL1C_20210724T190921_N0301_R056_T10UFB_20210724T230122.bin','S2B_MSIL1C_20210726T185919_N0301_R013_T10UFB_20210726T211239.bin','S2B_MSIL1C_20210729T190919_N0301_R056_T10UFB_20210729T212314.bin','S2A_MSIL1C_20210803T190921_N0301_R056_T10UFB_20210803T224926.bin','S2B_MSIL1C_20210805T185919_N0301_R013_T10UFB_20210805T211134.bin','S2A_MSIL1C_20210813T190921_N0301_R056_T10UFB_20210813T224901.bin','S2A_MSIL1C_20210902T190911_N0301_R056_T10UFB_20210902T225534.bin','S2B_MSIL1C_20210907T190929_N0301_R056_T10UFB_20210907T224046.bin']
 param_plots(filenames,.2)
+'''
