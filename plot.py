2e4cc5c285aff6b43908323c0f3993af8c52826b
Sterling von Dehn <sterlingvondehn@gmail.com>
2024-06-06 13:51:50 -0700

    updates

--- plot.py
+++ plot.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 import os
-
+import datetime
 
 def scale(X):
     # default: scale a band to [0, 1]  and then clip
@@ -18,7 +18,7 @@ def scale(X):
     if  True:
         values = X.ravel().tolist()
         values.sort()
-        n_pct = 1. # percent for stretch value
+        n_pct = .01 # percent for stretch value
         frac = n_pct / 100.
         lower = int(math.floor(float(len(values))*frac))
         upper = int(math.floor(float(len(values))*(1. - frac)))
@@ -28,13 +28,30 @@ def scale(X):
     return X
     
 
-def plot(file_list):
+def extract_date(file_name):
+    # Assuming the date format is YYYY-MM-DD in the file names
+    date  = file_name.split('_')[2].split('T')[0]
+    return datetime.datetime.strptime(date, "%Y%m%d")
+
+
+def plot(file_dir):
     '''
-    Takes a list of binary raster files organized by date and plots an image using the B12, B11, and B09 bands. Also plots the NBR of each frame as well as the dNBR of each frame except the first (first frame would have dNBR=0). Function places each files into three directories: 'images', 'NBR', and 'dNBR'.
-    >>> plot(['S2B_MSIL1C_20210626T185919_N0300_R013_T10UFB_20210626T211041.bin',...,'S2B_MSIL1C_20210907T190929_N0301_R056_T10UFB_20210907T224046.bin'])
+    Takes a directory containing bin files and plots an image using the B12, B11, and B09 bands. Also plots the NBR of each frame as well as the dNBR of each frame except the first (first frame would have dNBR=0). Function places each files into three directories: 'images', 'NBR', and 'dNBR'.
+    >>> plot('raster_data')
     '''
-    for n in range(len(file_list)):
-        vals = read_binary(f'raster_data/small/{file_list[n]}') #reading each file
+    #extracting bin files
+    files = os.listdir(file_dir)
+    file_list = []
+    for n in range(len(files)):
+        if files[n].split('.')[-1] == 'bin':
+            file_list.append(files[n])
+        else:
+            continue;
+        
+    sorted_file_names = sorted(file_list, key=extract_date) #sorting files by date
+
+    for n in range(len(sorted_file_names)):
+        vals = read_binary(f'{file_dir}/{sorted_file_names[n]}') #reading each file
         data = vals[3]
         width = vals[0]
         height = vals[1]
@@ -47,19 +64,16 @@ def plot(file_list):
             for j in range(width):
                 B12[i][j] = data[width*height*0 + width*i+j] #updating band data for each of the 4 bands
                 B11[i][j] = data[width*height*1 + width*i+j]
-                B09[i][j] = data[width*height*2 + width*i+j]
-                B08[i][j] = data[width*height*3 + width*i+j]
+                B08[i][j] = data[width*height*2 + width*i+j]
+                #B08[i][j] = data[width*height*3 + width*i+j]
         NBR = (B12-B08)/(B12+B08)      
         band1 = scale(B12) #scaling bands for plotting
         band2 = scale(B11)
         band3 = scale(B09)
         band4 = scale(B08)
-        date  = file_list[n].split('_')[2].split('T')[0]
-        image = np.stack([band1,band2,band3], axis=2) #creating 3D matrix for RGB plot
+        date  = sorted_file_names[n].split('_')[2].split('T')[0]
+        image = np.stack([band1,band2,band4], axis=2) #creating 3D matrix for RGB plot
         
-        plt.plot(nbr)
-        
-        '''
         plt.figure(figsize=(15,15)) #setting figure parameters
         imratio = height/width
         plt.imshow(image) #Plotting the image
@@ -67,7 +81,7 @@ def plot(file_list):
         if not os.path.exists('images'):
             os.mkdir('images')
         plt.tight_layout()
-        plt.savefig(f'images/{date}_{file_list[n]}.png')
+        plt.savefig(f'images/{date}_{sorted_file_names[n]}.png')
         plt.clf()
         
         plt.imshow(NBR, cmap='Greys') #Plotting the NBR
@@ -76,7 +90,7 @@ def plot(file_list):
         if not os.path.exists('NBR'):
             os.mkdir('NBR')
         plt.tight_layout()
-        plt.savefig(f'NBR/{date}_{file_list[n]}.png')
+        plt.savefig(f'NBR/{date}_{sorted_file_names[n]}.png')
         plt.clf()
         
         #Plotting the dNBR for all frames but the first
@@ -90,11 +104,5 @@ def plot(file_list):
             if not os.path.exists('dNBR'):
                 os.mkdir('dNBR')
             plt.tight_layout()
-            plt.savefig(f'dNBR/{date}_{file_list[n]}.png') 
+            plt.savefig(f'dNBR/{date}_{sorted_file_names[n]}.png') 
             plt.clf()
-        '''
-
-
-
-filenames = ['S2B_MSIL1C_20210626T185919_N0300_R013_T10UFB_20210626T211041.bin','S2B_MSIL1C_20210629T190919_N0300_R056_T10UFB_20210629T212050.bin','S2A_MSIL1C_20210701T185921_N0301_R013_T10UFB_20210701T223921.bin','S2B_MSIL1C_20210709T190919_N0301_R056_T10UFB_20210709T224644.bin','S2A_MSIL1C_20210714T190921_N0301_R056_T10UFB_20210714T225634.bin','S2B_MSIL1C_20210719T190919_N0301_R056_T10UFB_20210719T212141.bin','S2A_MSIL1C_20210724T190921_N0301_R056_T10UFB_20210724T230122.bin','S2B_MSIL1C_20210726T185919_N0301_R013_T10UFB_20210726T211239.bin','S2B_MSIL1C_20210729T190919_N0301_R056_T10UFB_20210729T212314.bin','S2A_MSIL1C_20210803T190921_N0301_R056_T10UFB_20210803T224926.bin','S2B_MSIL1C_20210805T185919_N0301_R013_T10UFB_20210805T211134.bin','S2A_MSIL1C_20210813T190921_N0301_R056_T10UFB_20210813T224901.bin','S2A_MSIL1C_20210902T190911_N0301_R056_T10UFB_20210902T225534.bin','S2B_MSIL1C_20210907T190929_N0301_R056_T10UFB_20210907T224046.bin']
-plot(filenames)
