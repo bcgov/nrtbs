45f89ef703f26c4fa3540375829c5fc0366b8419
Sterling von Dehn <sterlingvondehn@gmail.com>
2024-06-11 13:50:07 -0700

    updates

--- dNBR.py
+++ dNBR.py
@@ -57,42 +57,48 @@ def class_plot(start_file, end_file):
     dnbr = dNBR(start_file,end_file)
     scaled_dNBR = (dnbr*2000+275)/5 #scalling dNBR
     class_plot = np.zeros((len(scaled_dNBR),len(scaled_dNBR[0])))
+    un_tot = 0
+    low_tot = 0
+    med_tot = 0
+    high_tot = 0
     for i in range(len(scaled_dNBR)): #making classifications
         for j in range(len(scaled_dNBR[0])):
             if scaled_dNBR[i][j] < 76:
                 class_plot[i][j] = 0
+                un_tot += 1
             elif 76 <= scaled_dNBR[i][j] < 110:
                 class_plot[i][j] = 1
+                low_tot += 1
             elif 110 <= scaled_dNBR[i][j] < 187:
                 class_plot[i][j] = 2
+                med_tot += 1
             else:
                 class_plot[i][j] = 3
+                high_tot += 1
                 
     start = start_file.split('/')[-1] #splitting files for file names
     end = end_file.split('/')[-1]
     start_date  = start.split('_')[2].split('T')[0]
     end_date = end.split('_')[2].split('T')[0]
+    #calculating percentages of each class
+    tot = un_tot+low_tot+med_tot+high_tot
+    un_per = round(100*un_tot/tot,1)
+    low_per = round(100*low_tot/tot,1)
+    med_per = round(100*med_tot/tot,1)
+    high_per = round(100*high_tot/tot,1)
+    
+
     
     cmap = matplotlib.colors.ListedColormap(['green','yellow','orange','red'])   #plotting
-    imratio = len(scaled_dNBR)/len(scaled_dNBR[0])   
-    plt.figure(figsize=(15,15))       
+    plt.figure(figsize=(15,15))
     plt.imshow(class_plot,cmap=cmap)
     plt.title(f'BARC 256 burn severity, start date:{start_date}, end date:{end_date}')
     plt.xlabel(f'start file:{start}, end file:{end}')
-    plt.colorbar(fraction=0.04525*imratio)
-    plt.tight_layout()
+    plt.scatter(np.nan,np.nan,marker='s',s=100,label=f'Unburned {un_per}%',color='green')
+    plt.scatter(np.nan,np.nan,marker='s',s=100,label=f'Low {low_per}%' ,color='yellow')
+    plt.scatter(np.nan,np.nan,marker='s',s=100,label=f'Medium {med_per}%',color='orange')
+    plt.scatter(np.nan,np.nan,marker='s',s=100,label=f'High {high_per}%',color='red')
+    plt.legend(fontsize="20")
     plt.show()
     #plt.savefig(f'{end}_BARC_classification.png')
-    
     return class_plot
-
-
-'''
-diff = dNBR('pre/trim/S2A_MSIL1C_20200729T190921_N0209_R056_T10UFB_20200729T225849.bin','post/trim/S2B_MSIL1C_20220902T190919_N0400_R056_T10UFB_20220902T212431.bin') - dNBR('L1/trimmed/S2B_MSIL1C_20210626T185919_N0300_R013_T10UFB_20210626T211041.bin','L1/trimmed/S2B_MSIL1C_20210907T190929_N0301_R056_T10UFB_20210907T224046.bin')
-cmap = matplotlib.colors.ListedColormap(['green','yellow','orange','red'])   #plotting
-#imratio = len(scaled_dNBR)/len(scaled_dNBR[0])   
-#plt.figure(figsize=(15,15))       
-plt.imshow(diff,cmap=cmap)
-plt.colorbar()
-plt.show()
-'''
