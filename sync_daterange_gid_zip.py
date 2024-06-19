4fe7310d0ab2b9845d73e5bfd1ec7ac76d288371
SterlingvonDehn <sterlingvondehn@gmail.com>
2024-06-19 08:26:12 -0700

    Add files via upload

--- sync_daterange_gid_zip.py
+++ sync_daterange_gid_zip.py
@@ -14,7 +14,8 @@ import time
 import json
 import sys
 import os
-my_path = sep.join(os.path.abspath(__file__).split(sep)[:-1]) + sep
+
+#my_path = sep.join(os.path.abspath(__file__).split(sep)[:-1]) + sep
 product_target = os.getcwd() + sep # put ARD products into present folder
 
 def download_by_gids(gids, yyyymmdd, yyyymmdd2):
@@ -43,11 +44,13 @@ def download_by_gids(gids, yyyymmdd, yyyymmdd2):
                     '--bucket sentinel-products-ca-mirror'])
     print(cmd)
     data = os.popen(cmd).read()
-
+    
+    '''
     if not exists(my_path + 'listing'):  # json backup for analysis
         os.mkdir(my_path + 'listing')
     df = my_path + 'listing' + sep + ts + '_objects.txt'  # file to write
     open(df, 'wb').write(data.encode())  # record json to file
+    '''
 
     jobs, d = [], None
     try:
