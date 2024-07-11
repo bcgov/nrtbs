5cfdfe4ceb776f608dddd7e4c045948d120c766d
Sterling von Dehn <sterlingvondehn@gmail.com>
2024-07-11 14:02:48 -0700

    updates

--- py/cut.py
+++ py/cut.py
@@ -4,20 +4,21 @@ from misc import run, err, exist, args
 from envi import envi_header_cleanup, envi_header_copy_bandnames
 import os
 
+def cut(fn, A, B, C, D):
+
+    out_fn = f'{fn.strip(".bin")}_cut.bin'
+    out_fn_hdr = f'{fn.strip(".bin")}_cut.hdr'
+
+    run(f'gdal_translate -of ENVI -ot Float32 -srcwin { (" ".join([A, B, C, D]))} {fn} {out_fn}')  # output file
+
+    envi_header_copy_bandnames(['',fn[:-4] + '.hdr', out_fn_hdr])
+    
+
 if len(args) < 6:
     err('cut.py [src image or dir] [gdal translate -srcwin parameter 1] [-srcwin param 2] [ -srcwin param 3] # cut image with GDAL and cleanup headers 20220814')
     
 A, B, C, D = args[2: 6]
 fn = args[1]
-
-def cut(fn, A, B, C, D):
-
-    out_fn = f'{fn.strip('.bin')}_cut.bin'
-    out_fn_hdr = f'{fn.strip('.bin')}_cut.hdr'
-
-    run(f'gdal_translate -of ENVI -ot Float32 -srcwin { (' '.join([A, B, C, D]))} {fn} {out_fn}')  # output file
-
-    envi_header_copy_bandnames(['',fn[:-4] + '.hdr', out_fn_hdr])
 
 if __name__ == "__main__":
     
