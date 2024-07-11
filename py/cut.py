48f60a95ae88bec972293ab2ee6cd08896751ffe
Sterling von Dehn <sterlingvondehn@gmail.com>
2024-07-11 10:33:15 -0700

    updates

--- py/cut.py
+++ py/cut.py
@@ -5,7 +5,7 @@ from envi import envi_header_cleanup, envi_header_copy_bandnames
 import os
 
 if len(args) < 6:
-    err('cut.py [src image] [gdal translate -srcwin parameter 1] [-srcwin param 2] [ -srcwin param 3] # cut image with GDAL and cleanup headers 20220814')
+    err('cut.py [src image or dir] [gdal translate -srcwin parameter 1] [-srcwin param 2] [ -srcwin param 3] # cut image with GDAL and cleanup headers 20220814')
     
 A, B, C, D = args[2: 6]
 fn = args[1]
@@ -14,7 +14,6 @@ def cut(fn, A, B, C, D):
 
     out_fn = f'{fn.strip('.bin')}_cut.bin'
     out_fn_hdr = f'{fn.strip('.bin')}_cut.hdr'
-    print(f' out file name: !!!!!! {fn}')
 
     run(f'gdal_translate -of ENVI -ot Float32 -srcwin { (' '.join([A, B, C, D]))} {fn} {out_fn}')  # output file
 
