c57a3eaca5c25bd8b6601b6e3027e6992e849d51
Sterling von Dehn <sterlingvondehn@gmail.com>
2024-08-13 09:01:45 -0700

    updates

--- py/get_composite.py
+++ py/get_composite.py
@@ -1,7 +1,9 @@
 '''
 donwloads sentinel data, extracts cloudfree swir nir bands, choses most recent avaialbe pixle, and merges frames (if necesary) for the given fire and date range
-$python3 get_composite.py 20240601 20240630 G90267 
-$python3 get_composite.py 20240701 20240812 N51117 N51069 N51210 N51103
+$python3 get_composite.py G90267 
+$python3 get_composite.py 20240630 G90267 
+$python3 get_composite.py N51117 N51069 N51210 N51103
+$python3 get_composite.py 20240810 N51117 N51069 N51210 N51103
 '''
 from percent_vs_time import extract_data_percent
 from misc import run, args, extract_date
@@ -13,28 +15,56 @@ import geopandas as gpd
 from datetime import datetime, timedelta
 from dnbr import time_series
 
-def get_composite_image(start_date, end_date, fire_num):
+def get_composite_image(fire_num, end_date=None):
     '''
     Takes a fire number as well as a tile ID and downloads an MRAP timesires composite
     '''
+    #changing single fire numbers to list and naming multi fire scene
+    if type(fire_num) == list:
+        fire_name = f'{fire_num[0]}_complex'
+        fire_nums = fire_num
+    else:
+        print(fire_num)
+        fire_name = fire_num
+        fire_nums = [fire_num]
+    
+    #taking end date as today none defined
+    if end_date == None:
+        end_date = datetime.today().date()
+        str_end_date_comps = str(end_date).split('-')
+        str_end_date = ''
+        for comp in str_end_date_comps:
+            str_end_date += comp
+    else:
+        str_end_date = end_date
+    
+    #getting the ignition date for fires and taking the smallest
+    fire_perims_path = '../shape_files/prot_current_fire_points.shp'
+    fire_perims = gpd.read_file(fire_perims_path)
+    fire_perims = fire_perims.to_crs(epsg=4326)
+    fire_num_perim = fire_perims[fire_perims['FIRE_NUM'].isin(fire_nums)]
+    ignt_dates = [datetime.strptime(str(date), '%Y-%m-%d %H:%M:%S').date() for date in  fire_num_perim.IGNITN_DT]
+    fire_start_date = min(ignt_dates)
+    start_date = fire_start_date - timedelta(weeks=3)
+
+    #changing date format to string for sync_daterange script
+    str_start_date_comps = str(start_date).split('-')
+    str_start_date = ''
+    for comp in str_start_date_comps:
+        str_start_date += comp
+
     tiles = check_tile_id(fire_num) #checking tiles
     tile_str = ''
     for tile in tiles:
         tile_str += f' {tile}'
-    sync_string = f'python3 sync_daterange_gid_zip.py {start_date} {end_date}' + tile_str
-
-    if type(fire_num) == str:
-        fire_name = fire_num
-        fire_nums = [fire_num]
-    else:
-        fire_name = f'{fire_num[0]}_complex'
-        fire_nums = fire_num
-
-    #run(sync_string) #running download script
+    
+    sync_string = f'python3 sync_daterange_gid_zip.py {str_start_date} {str_end_date}' + tile_str #defining sync string
+    run(sync_string) #running download script
     run('python3 sentinel2_extract_cloudfree_swir_nir.py') #running cloudfree extraction
     run('python3 sentinel2_mrap.py') #running MRAP script
     if len(tiles) > 1:
         run(f'python3 sentinel2_mrap_merge.py {fire_name}') #running merge script if necesary 
+
     #renaming directory and moving non MRAP frames
     else:
         os.rename(f'L2_{tiles[0]}', fire_name)
@@ -48,14 +78,6 @@ def get_composite_image(start_date, end_date, fire_num):
     cut_data = plot_image_with_rectangle(files[-1]) #prompt user for cut coords
     run(f'python3 cut.py {fire_name} {int(cut_data[0])} {int(cut_data[1])} {int(cut_data[2])} {int(cut_data[3])}')
 
-    #getting the ignition date for fires and taking the smallest
-    fire_perims_path = '../shape_files/prot_current_fire_points.shp'
-    fire_perims = gpd.read_file(fire_perims_path)
-    fire_perims = fire_perims.to_crs(epsg=4326)
-    fire_num_perim = fire_perims[fire_perims['FIRE_NUM'].isin(fire_nums)]
-    ignt_dates = [datetime.strptime(str(date), '%Y-%m-%d %H:%M:%S').date() for date in  fire_num_perim.IGNITN_DT]
-    start_date = min(ignt_dates)
-
     #reading through files to find nearest date to start date
     files = os.listdir(f'{fire_name}_cut')
     date_list = []
@@ -66,7 +88,7 @@ def get_composite_image(start_date, end_date, fire_num):
             continue
     date_list = sorted(date_list)
     for i in range(len(date_list)):
-        if datetime.strptime(date_list[i], '%Y%m%d').date() >= (start_date - timedelta(days=1)):
+        if datetime.strptime(date_list[i], '%Y%m%d').date() >= (fire_start_date - timedelta(days=1)):
             barc_start = date_list[i-1]
             break
 
@@ -76,8 +98,8 @@ def get_composite_image(start_date, end_date, fire_num):
     
 if __name__ == "__main__":
 
-    if len(args) == 4: # if 1 fire given
-        get_composite_image(args[1],args[2],args[3])
-
-    else: # if multiple fires given
-        get_composite_image(args[1],args[2],args[3:-1])
+    if len(args[1]) == 8: #runs with specified end date if one is provided
+        get_composite_image(args[2:], args[1])
+    
+    else: #runs with end date set to current date
+        get_composite_image(args[1:])
