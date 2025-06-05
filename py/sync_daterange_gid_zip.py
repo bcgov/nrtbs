3eac1904361cf925bfc7e8039b04ea8362ae459e
ashlin richardson <ashlin.richardson@gmail.com>
2025-06-05 09:30:36 -0700

    update

--- py/sync_daterange_gid_zip.py
+++ py/sync_daterange_gid_zip.py
@@ -10,7 +10,9 @@ if not use_L2:
 no_update_listing = False # flag to skip refreshing listing of aws archive
 
 from misc import args, sep, exists, parfor, run, timestamp, err
+from aws_download import aws_download
 import multiprocessing as mp
+from pathlib import Path
 import datetime
 import time
 import json
@@ -102,25 +104,35 @@ def download_by_gids(gids, yyyymmdd, yyyymmdd2):
             if not os.path.exists(out_dir):
                 os.mkdir(out_dir)
 
+            '''
             cmd = ' '.join(['aws',
                             's3',
                             'cp',
-                            '--no-verify-ssl',
                             '--no-sign-request',
                             's3://sentinel-products-ca-mirror/' + key,
                             f])
-     
-            if exists(f):
+            print([f])'''
+            if exists(f) and Path(f).stat().st_size == file_size:
                 print(f, "SKIPPING")
             else:
                 print(f)
-                cmds += [cmd]
-    
+                #cmds += [cmd]
+
+                aws_download('sentinel-products-ca-mirror',
+                             key,
+                             Path(f)) # LOCAL_PATH):
+                # === CONFIGURATION ===
+                # BUCKET = "sentinel-products-ca-mirror"
+                # KEY = "Sentinel-2/S2MSI2A/2025/05/27/S2C_MSIL2A_20250527T191931_N0511_R099_T10VFL_20250528T002013.zip"
+                # LOCAL_PATH = Path("L2_T10VFL") / Path(KEY).name
+
+'''
     print(cmds)
     def runc(c):
         print([c])
         return os.system(c)
     parfor(runc, cmds, int(mp.cpu_count()))  
+'''
 
 gids = []  # get gids from command line
 if len(args) > 3:
